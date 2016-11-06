ef1c9ca99ffc70fe811aaf32f9e3b3cf91964d80
Carlos Santana <csantana@us.ibm.com>
2016-11-06 12:17:53 -0500

    update provider action limits

--- install.sh
+++ install.sh
@@ -8,4 +8,4 @@ source .env
 
 wsk package create couchdb 
 wsk action create couchdb/changes feed/changes.js -p mgmtdbUrl ${DB_URL} -p mgmtdbName ${DB_NAME_TRIGGERS} -p endpoint $(wsk property get --apihost | awk '{printf $4}')
-wsk action create couchdb/provider feed/provider.js -p mgmtdbUrl ${DB_URL} -p mgmtdbName ${DB_NAME_TRIGGERS}
+wsk action create couchdb/provider feed/provider.js -p mgmtdbUrl ${DB_URL} -p mgmtdbName ${DB_NAME_TRIGGERS} -t 300000 -m 512
